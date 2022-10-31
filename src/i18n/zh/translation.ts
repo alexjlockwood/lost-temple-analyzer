3301adb38cdfb0c3795ec7a9a4eb305cb93d8841
Alex Lockwood <alexjlockwood@gmail.com>
2022-10-31 13:55:11 -0400

    update translations

--- src/i18n/zh/translation.ts
+++ src/i18n/zh/translation.ts
@@ -3,8 +3,7 @@ export const zh = {
     description: '点按并拖动下方的路线图来查看以此为前提的剩余门正确概率',
     resetGridButton: '重置路线图',
     copyLinkButton: '拷贝路线链接',
-    // TODO: translate these
-    copyLinkToClipboardSuccess: 'Link copied to clipboard',
-    copyLinkToClipboardError: 'Unable to copy link to clipboard',
+    copyLinkToClipboardSuccess: '已拷贝链接到剪贴板',
+    copyLinkToClipboardError: '拷贝链接到剪贴板时出错',
   },
 };
