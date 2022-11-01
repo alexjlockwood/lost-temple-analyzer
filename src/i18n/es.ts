79ac2150b8ef828422ad2eed4e26ae421ecb8985
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-01 18:15:21 -0400

    update spanish translation

--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -1,7 +1,7 @@
 export const es = {
   common: {
     description:
-      'Haz click o arrastre los cuartos y las puertas a continuación para ver las probabilidades de los diferentes patrones',
+      'Haz click o arrastre los cuartos y las puertas a continuación para ver las probabilidades de los diferentes patrones. Haz click una vez en una puerta para marcarla como abierta, dos veces para marcarla como cerrada y tres veces para borrarla.      ',
     resetGridButton: 'Restablecer cuadrícula',
     copyLinkButton: 'Copiar link',
     copyLinkToClipboardSuccess: 'Link copiado al portapapeles',
