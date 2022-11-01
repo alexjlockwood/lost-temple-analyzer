ab62e903fcaf929249464eaab29f0e0ca3c67d76
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-01 15:39:52 -0400

    update korean translation

--- src/i18n/ko/translation.ts
+++ src/i18n/ko/translation.ts
@@ -4,11 +4,10 @@ export const ko = {
       '방이나 통로를 마우스로 클릭하거나 드래그하여 각기 다른 길의 존재 가능성을 확인하세요',
     resetGridButton: '재시작',
     copyLinkButton: '주소 복사',
-    // TODO: translate these
-    copyLinkToClipboardSuccess: 'Link copied to clipboard',
-    copyLinkToClipboardError: 'Unable to copy link to clipboard',
-    possiblePaths_one: '{{count}} possible path',
-    possiblePaths_other: '{{count}} possible paths',
-    viewOnLargerScreen: 'View on a larger screen to see the list of possible paths',
+    copyLinkToClipboardSuccess: '링크가 클립보드에 복사되었습니다',
+    copyLinkToClipboardError: '링크를 클립보드에 복사할 수 없습니다',
+    possiblePaths_one: '{{count}}개의 가능한 경로',
+    possiblePaths_other: '{{count}}개의 가능한 경로',
+    viewOnLargerScreen: '가능한 경로 목록을 보려면 더 큰 화면으로 확대하세요',
   },
 };
