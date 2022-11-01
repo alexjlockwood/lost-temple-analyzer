c853b7143b651dd29180ca16cfd5bcfbc5bb967d
Alex Lockwood <alexjlockwood@gmail.com>
2022-10-31 23:14:42 -0400

    update translations

--- src/i18n/zh/translation.ts
+++ src/i18n/zh/translation.ts
@@ -1,13 +1,12 @@
 export const zh = {
   common: {
-    description: '点按并拖动下方的路线图来查看以此为前提的剩余门正确概率',
+    description: '点击或拖动来与下方的路线图交互，查看不同情况下剩余门的正确概率',
     resetGridButton: '重置路线图',
     copyLinkButton: '拷贝路线链接',
     copyLinkToClipboardSuccess: '已拷贝链接到剪贴板',
     copyLinkToClipboardError: '拷贝链接到剪贴板时出错',
     possiblePaths_one: '{{count}} 种可能的路线',
     possiblePaths_other: '{{count}} 种可能的路线',
-    // TODO: translate this
-    viewOnLargerScreen: 'View on a larger screen to see the list of possible paths',
+    viewOnLargerScreen: '要查阅可能的路线列表需要在更大分辨率的屏幕上浏览本网页',
   },
 };
