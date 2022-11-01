f8546f3449a988d0396425cad25696cc55718ef5
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-01 16:40:30 -0400

    add french

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,12 +3,13 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import { en } from './en/translation';
 import { es } from './es/translation';
+import { fr } from './fr/translation';
 import { ko } from './ko/translation';
 import { sv } from './sv/translation';
 import { zh } from './zh/translation';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
-  resources: { en, es, ko, sv, zh },
+  resources: { en, es, fr, ko, sv, zh },
   defaultNS: 'common',
   fallbackLng: 'en',
 });
---
+++ src/i18n/fr/translation.ts
@@ -0,0 +1,13 @@
+export const fr = {
+  common: {
+    description:
+      'Cliquez ou faites glisser les salles et portes en-dessous pour voir les probabilités des différents chemins. Cliquez une fois sur une porte pour la marquer comme ouverte, deux fois pour la marquer comme fermée, et trois fois pour réinitialiser la porte.',
+    resetGridButton: 'Réinitialiser la grille',
+    copyLinkButton: 'Copier le lien',
+    copyLinkToClipboardSuccess: 'Lien copié dans le presse-papiers',
+    copyLinkToClipboardError: 'Impossible de copier le lien dans le presse-papiers',
+    possiblePaths_one: '{{count}} chemin possible',
+    possiblePaths_other: '{{count}} chemins possibles',
+    viewOnLargerScreen: 'Vue sur un menu plus large pour voir la liste des chemins possibles',
+  },
+};
