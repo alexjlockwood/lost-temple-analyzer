24766a3f9a62eb7252530386aaecf1df14b1c691
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-01 16:43:23 -0400

    reorganize

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,12 +1,12 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import { en } from './en/translation';
-import { es } from './es/translation';
-import { fr } from './fr/translation';
-import { ko } from './ko/translation';
-import { sv } from './sv/translation';
-import { zh } from './zh/translation';
+import { en } from './en';
+import { es } from './es';
+import { fr } from './fr';
+import { ko } from './ko';
+import { sv } from './sv';
+import { zh } from './zh';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
   resources: { en, es, fr, ko, sv, zh },
