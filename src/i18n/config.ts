a915238b456838e298f179f9033a5b36f3f1ed6b
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-01 15:44:43 -0400

    add swedish

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -4,10 +4,11 @@ import { initReactI18next } from 'react-i18next';
 import { en } from './en/translation';
 import { es } from './es/translation';
 import { ko } from './ko/translation';
+import { sv } from './sv/translation';
 import { zh } from './zh/translation';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
-  resources: { en, es, ko, zh },
+  resources: { en, es, ko, sv, zh },
   defaultNS: 'common',
   fallbackLng: 'en',
 });
---
+++ src/i18n/sv/translation.ts
@@ -0,0 +1,13 @@
+export const sv = {
+  common: {
+    description:
+      'Klicka eller dra de rummen och dörrarna under för att visa den dörrens chans av andra vägar',
+    resetGridButton: 'Börja om rutnät',
+    copyLinkButton: 'Kopiera länken',
+    copyLinkToClipboardSuccess: 'Länken kopierad till urklipp',
+    copyLinkToClipboardError: 'Går inte att kopiera till urklipp',
+    possiblePaths_one: '{{count}} möjlig väg',
+    possiblePaths_other: '{{count}} möjliga vägar',
+    viewOnLargerScreen: 'Visa på en större skärm för att se listan av olika vägar',
+  },
+};
