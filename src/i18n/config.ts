05d03f2ed5a5a634ca44e368264347e4691d84fe
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-01 13:25:53 -0400

    add korean translation

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,10 +3,11 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import { en } from './en/translation';
 import { es } from './es/translation';
+import { ko } from './ko/translation';
 import { zh } from './zh/translation';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
-  resources: { en, es, zh },
+  resources: { en, es, ko, zh },
   defaultNS: 'common',
   fallbackLng: 'en',
 });
---
+++ src/i18n/ko/translation.ts
@@ -0,0 +1,14 @@
+export const ko = {
+  common: {
+    description:
+      '방이나 통로를 마우스로 클릭하거나 드래그하여 각기 다른 길의 존재 가능성을 확인하세요',
+    resetGridButton: '재시작',
+    copyLinkButton: '주소 복사',
+    // TODO: translate these
+    copyLinkToClipboardSuccess: 'Link copied to clipboard',
+    copyLinkToClipboardError: 'Unable to copy link to clipboard',
+    possiblePaths_one: '{{count}} possible path',
+    possiblePaths_other: '{{count}} possible paths',
+    viewOnLargerScreen: 'View on a larger screen to see the list of possible paths',
+  },
+};
