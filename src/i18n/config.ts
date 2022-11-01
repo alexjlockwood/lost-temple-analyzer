516f9b154239210381944ac428f51fc880cd933a
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-01 17:14:47 -0400

    add finnish

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -4,12 +4,13 @@ import { initReactI18next } from 'react-i18next';
 import { en } from './en';
 import { es } from './es';
 import { fr } from './fr';
+import { fi } from './fi';
 import { ko } from './ko';
 import { sv } from './sv';
 import { zh } from './zh';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
-  resources: { en, es, fr, ko, sv, zh },
+  resources: { en, es, fi, fr, ko, sv, zh },
   defaultNS: 'common',
   fallbackLng: 'en',
 });
---
+++ src/i18n/fi.ts
@@ -0,0 +1,14 @@
+export const fi = {
+  common: {
+    description:
+      'Klikkaa tai raahaa huoneita ja ovia alla olevassa kuvassa nähdäksesi eri reittien todennäköisyydet. Merkitse ovi aukeavaksi klikkaamalla sitä kerran, suljetuksi klikkaamalla kahdesti, ja poista merkintä klikkaamalla kolmannen kerran.',
+    resetGridButton: 'Tyhjennä kaikki',
+    copyLinkButton: 'Kopioi linkki',
+    copyLinkToClipboardSuccess: 'Linkki kopioitu leikepöydälle',
+    copyLinkToClipboardError: 'Linkin kopiointi leikepöydälle epäonnistui',
+    possiblePaths_one: '{{count}} mahdollinen reitti',
+    possiblePaths_other: '{{count}} mahdollista reittiä',
+    viewOnLargerScreen:
+      'Tarkastele sivua suuremmalla näytöllä tai selainikkunalla nähdäksesi kaikki mahdolliset reitit',
+  },
+};
