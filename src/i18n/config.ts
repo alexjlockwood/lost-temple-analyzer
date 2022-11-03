deca7d3405a495b462f08716ac8c2b80b5ea1048
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-03 17:19:21 -0400

    add indonesian

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,14 +3,15 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import { en } from './en';
 import { es } from './es';
-import { fr } from './fr';
 import { fi } from './fi';
+import { fr } from './fr';
+import { id } from './id';
 import { ko } from './ko';
 import { sv } from './sv';
 import { zh } from './zh';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
-  resources: { en, es, fi, fr, ko, sv, zh },
+  resources: { en, es, fi, fr, id, ko, sv, zh },
   defaultNS: 'common',
   fallbackLng: 'en',
 });
