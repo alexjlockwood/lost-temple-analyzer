43d1bb0cf8a92082bb176215f89788f90355199a
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-03 17:17:03 -0400

    add indonesian

---
+++ src/i18n/id.ts
@@ -0,0 +1,13 @@
+export const id = {
+  common: {
+    description:
+      'Klik untuk memilih ruang dan pintu di bawah untuk melihat angka probabilitas masing-masing jalur. Klik pintu satu kali jika pintu itu berhasil terbuka, klik dua kali jika gagal, klik tiga kali untuk mengulang dari awal.',
+    resetGridButton: 'Ulangi Dari Awal',
+    copyLinkButton: 'Salin link',
+    copyLinkToClipboardSuccess: 'Link berhasil disalin',
+    copyLinkToClipboardError: 'Link tidak berhasil disalin',
+    possiblePaths_one: '{{count}} kemungkinan jalur.',
+    possiblePaths_other: '{{count}} kemungkinan jalur.',
+    viewOnLargerScreen: 'Lihat daftar kemungkinan jalur di layar yang lebih besar.',
+  },
+};
