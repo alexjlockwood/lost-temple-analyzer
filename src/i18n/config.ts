087654abd19fde1d9651c8fce3135d184753afb7
Alex Lockwood <alexjlockwood@gmail.com>
2022-11-04 11:38:49 -0400

    add dutch

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -7,11 +7,12 @@ import { fi } from './fi';
 import { fr } from './fr';
 import { id } from './id';
 import { ko } from './ko';
+import { nl } from './nl';
 import { sv } from './sv';
 import { zh } from './zh';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
-  resources: { en, es, fi, fr, id, ko, sv, zh },
+  resources: { en, es, fi, fr, id, ko, nl, sv, zh },
   defaultNS: 'common',
   fallbackLng: 'en',
 });
---
+++ src/i18n/nl.ts
@@ -0,0 +1,13 @@
+export const nl = {
+  common: {
+    description:
+      'Klik of sleep de kamers en deuren hieronder om de verschillende waarschijnlijkheden te bekijken. Klik één keer op een deur om deze als open te markeren, twee keer voor gesloten en drie keer om te wissen.',
+    resetGridButton: 'Raster resetten',
+    copyLinkButton: 'Kopieer link',
+    copyLinkToClipboardSuccess: 'Link gekopieerd naar klembord',
+    copyLinkToClipboardError: 'Niet mogelijk om link naar klembord te kopieren',
+    possiblePaths_one: '{{count}} mogelijke route',
+    possiblePaths_other: '{{count}} mogelijke routes',
+    viewOnLargerScreen: 'Groter scherm weergave om de lijst met mogelijke routes te bekijken',
+  },
+};
