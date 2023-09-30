d79b7a8c3e0bce022cb17452fdcce46a29b27a01
Alex Lockwood <alexjlockwood@gmail.com>
2023-09-30 14:26:40 -0400

    Update italian translation

--- src/i18n/it.ts
+++ src/i18n/it.ts
@@ -1,8 +1,8 @@
 export const it = {
   common: {
     description:
-      'Clicca o trascina le stanze e le porte sottostanti per vedere le probabilità dei diversi percorsi. Clicca una porta una volta per marcarla come aperta, due volte per marcarla come chiusa e tre volte per ripristinare.',
-    resetGridButton: 'Resetta la griglia',
+      'Clicca o trascina le stanze e le porte sottostanti per vedere le probabilità dei diversi percorsi. Clicca una porta una volta per segnarla come aperta, due volte per segnarla come chiusa, e tre volte per ripristinarla.',
+    resetGridButton: 'Ripristina la griglia',
     copyLinkButton: 'Copia il link',
     copyLinkToClipboardSuccess: 'Link copiato negli appunti',
     copyLinkToClipboardError: 'Impossibile copiare il link negli appunti',
@@ -11,6 +11,6 @@ export const it = {
     possiblePaths_two: '{{count}} possibili percorsi',
     possiblePaths_threeToTen: '{{count}} possibili percorsi',
     possiblePaths_other: '{{count}} possibili percorsi',
-    viewOnLargerScreen: `Guarda da uno schermo più grande per vedere l'elenco dei possibili percorsi`,
+    viewOnLargerScreen: `Visualizza da uno schermo più grande per vedere anche l'elenco dei possibili percorsi`,
   },
 };
